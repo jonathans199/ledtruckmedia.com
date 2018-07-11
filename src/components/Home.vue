5650242bc58abc3027937f44606353c0c65c8332
mario diaz <mdiaz00147@gmail.com>
2018-07-11 10:51:21 -0400

    fix form and video

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -178,42 +178,35 @@ export default {
       show: true
     }
   },
-
-  
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        message: null,
+        checked: []
+      },
+      show: true
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset (evt) {
+      evt.preventDefault();
+      /* Reset our form values */
+      this.form.email = '';
+      this.form.name = '';
+      this.form.message = null;
+      this.form.checked = [];
+      /* Trick to reset/clear native browser form validation state */
+      this.show = false;
+      this.$nextTick(() => { this.show = true });
+    }
+  } 
 }
-
-
-// export default {
-//   data () {
-//     return {
-//       form: {
-//         email: '',
-//         name: '',
-//         message: null,
-//         checked: []
-//       },
-//       show: true
-//     }
-//   },
-//   methods: {
-//     onSubmit (evt) {
-//       evt.preventDefault();
-//       alert(JSON.stringify(this.form));
-//     },
-//     onReset (evt) {
-//       evt.preventDefault();
-//       /* Reset our form values */
-//       this.form.email = '';
-//       this.form.name = '';
-//       this.form.message = null;
-//       this.form.checked = [];
-//       /* Trick to reset/clear native browser form validation state */
-//       this.show = false;
-//       this.$nextTick(() => { this.show = true });
-//     }
-//   }
-// }
-
 
 </script>
 
