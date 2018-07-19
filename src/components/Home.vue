644cfd563db40e22be9b1b7a8e6b794f1d486de6
Jonathan Sanchez <jso1919@gmail.com>
2018-07-19 13:53:32 -0400

    fixed phone number and contact info

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,7 +16,7 @@
         </a>
       </div>
       <div class="header__menu-right text-center">
-        <a href="tel:917-224-3336">917-224-3336</a>&nbsp;
+        <a href="tel:917-224-3633">917-224-3633</a>&nbsp;
         <a class="fab fa-facebook-square" href="https://www.facebook.com/LED-Truck-Media-1575325705912466/" target="_blank"></a>&nbsp;
         <a class="fab fa-instagram" href="https://www.instagram.com/ledtruckmedia/" target="_blank"></a>&nbsp;
         <a class="fab fa-youtube" href="https://www.youtube.com/channel/UCqviwhdR9IIxiOHTpalhhLw" target="_blank"></a>
@@ -32,7 +32,7 @@
           </a>
         </div>
         <div class="col-12">
-          <a href="tel:917-224-3336">917-224-3336</a>&nbsp;
+          <a href="tel:917-224-3633">917-224-3633</a>&nbsp;
           <a class="fab fa-facebook-square" href="https://www.facebook.com/LED-Truck-Media-1575325705912466/" target="_blank"></a>&nbsp;
           <a class="fab fa-instagram" href="https://www.instagram.com/ledtruckmedia/" target="_blank"></a>&nbsp;
           <a class="fab fa-youtube" href="https://www.youtube.com/channel/UCqviwhdR9IIxiOHTpalhhLw" target="_blank"></a>
@@ -45,7 +45,7 @@
 
         <b-col class="text-center">
           <h1>YOUR ADS ON OUR TRUCKS</h1>
-          <button class="home__button" v-on:click="goToByScroll(10)" >Request A Quote!</button>
+          <button class="home__button" v-on:click="goToScroll(10)" >Request A Quote!</button>
         </b-col>
        
       </b-row>
@@ -111,7 +111,7 @@
         </div>
       </b-row>
       <b-row class="text-center statistics__contact">
-        <button v-on:click="goToByScroll(10)" >CONTACT US TODAY</button>
+        <button v-on:click="goToScroll(10)" >CONTACT US TODAY</button>
       </b-row>
     </b-container>
   </section>
@@ -121,10 +121,9 @@
       <div class="divider-1__container">
         <b-row>
           <div class="col-12">
-            <a class="mailto" style="padding: 10px;" href="mailto:sales@ledtruckmedia.com">sales@ledtruckmedia.com</a>
-
+            <a class="divider-1__phone" href="mailto:sales@ledtruckmedia.com"><h3>sales@ledtruckmedia.com</h3></a>
             <div class="divider-1__content col-12  text-center">
-              <h1 class="num">917-224-3336</h1>
+              <h1 class="num">917-224-3633</h1>
             </div>
             <h4 class="divider-1__address">400 NW 26 ST, Miami, Fl 33127</h4>
           </div>
@@ -219,7 +218,7 @@
         </b-col>
         <b-col class="md-6 my-5">
           <strong>Get in touch</strong>
-            <p>917.224.3336</p> 
+            <p>917.224.3633</p> 
         </b-col>
       </b-row>
     </b-container>
@@ -294,6 +293,7 @@ export default {
         this.onReset(evt)
       })
       .catch(function (error) {
+
       })
     },
 
@@ -302,6 +302,7 @@ export default {
       /* Reset our form values */
       this.form.email = '';
       this.form.name = '';
+      this.form.phone = '';
       this.form.message = null;
       this.form.checked = [];
       /* Trick to reset/clear native browser form validation state */
@@ -314,6 +315,11 @@ export default {
 </script>
 
 <style scoped>
+
+a:hover {
+  color: #b7b7b7 !important;
+  text-decoration: none;
+}
 
 .trucks__section--otheroptions {
   margin-top: 80px
@@ -400,12 +406,8 @@ export default {
   height: 100%;
   background-image: url("../assets/img/stats_bg.png");
   color: #eee;
+  background-size: cover;
 }
-
-.trucks___section--fulltruck {
-  
-}
-
 
 .boxes-3 {
   z-index: 4;
@@ -427,14 +429,6 @@ export default {
 .btn {
   border-color: white;
   border-radius: 1px;
-}
-
-.boxes-3__truck-side {
-
-}
-
-.boxes-3__truck-back {
-
 }
 
 .boxes-3__truck-full {
@@ -484,9 +478,18 @@ export default {
   z-index: 0;
 }
 
+.divider-1__phone h3 {
+  font-weight: 100;
+  letter-spacing: 7px;
+  padding: 1rem;
+  opacity: 1.4;
+  align-content: center;
+}
+
 .divider-1__address {
-  padding: 10px;
-  background-color: black;
+  font-weight: 100;
+  letter-spacing: 7px;
+  padding: 1rem;
   opacity: 1.4;
   /* padding-bottom: 10px; */
   align-content: center
@@ -502,12 +505,12 @@ export default {
 
 .divider-1__content {
   padding: 10px;
-  background-color: white;
-  color: black;
+  background-color: #2929298c;
+  color: white;
   width: 100%;
   padding-left: 40px;
   padding-right: 40px;
-  border-radius: 25px;
+  
 }
 
 .divider-1 {
